e39e201777dfcbb23339adca409cc321d0de056a
Lucas Anthony Coutts <a1922024@ingg22w060.ad.adelaide.edu.au>
2024-10-25 16:24:30 +1030

    .

--- Helper.hpp
+++ Helper.hpp
@@ -7,7 +7,7 @@ class Helper {
     static double absoluteSquaredDistance(std::tuple<int,int> item1, std::tuple<int,int> item2) {
         double xdist = std::get<0>(item1) - std::get<0>(item2);
         double ydist = std::get<1>(item1) - std::get<1>(item2);
-        return sqrt(xdist*xdist + ydist*ydist);
+        return pow(xdist*xdist + ydist*ydist,2);
     }
 
 };
