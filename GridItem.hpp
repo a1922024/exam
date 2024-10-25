b6bf2d4cbe2a146bd6256d8adfa64e5fea60c719
Lucas Anthony Coutts <a1922024@ingg22w060.ad.adelaide.edu.au>
2024-10-25 16:26:51 +1030

    .

--- GridItem.hpp
+++ GridItem.hpp
@@ -3,12 +3,12 @@
 #include<tuple>
 
 class GridItem {
+    static int itemCount;
     protected:
     std::tuple<int, int> position;
     int width;
     int height;
     public:
-    static int itemCount;
     GridItem(int x = 0, int y = 0, int width = 0, int height = 0)  {
         position = std::make_tuple(x,y);
         this -> width = width;
--- Helper.hpp
+++ Helper.hpp
@@ -7,7 +7,7 @@ class Helper {
     static double absoluteSquaredDistance(std::tuple<int,int> item1, std::tuple<int,int> item2) {
         double xdist = std::get<0>(item1) - std::get<0>(item2);
         double ydist = std::get<1>(item1) - std::get<1>(item2);
-        return pow(xdist*xdist + ydist*ydist,2);
+        return pow(abs(xdist) + abs(ydist),2);
     }
 
 };
